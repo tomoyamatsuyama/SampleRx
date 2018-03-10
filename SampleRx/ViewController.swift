d5d7d01835f6e1c395fbe4e2ee9ceaf90497d95d
Tomoya Matsuyama <m.tomoya0221@gmail.com>
2018-03-10 13:57:23 +0900

    first commit

---
+++ SampleRx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleRx
+//
+//  Created by Tomoya Matsuyama on 2018/03/10.
+//  Copyright © 2018年 Tomoya Matsuyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
